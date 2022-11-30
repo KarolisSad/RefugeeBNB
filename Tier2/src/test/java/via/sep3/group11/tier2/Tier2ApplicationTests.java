3d15702933fd7f7af5685a3b5078a9311eaec8b6
Christian Hougaard Pedersen <christianhougaardpedersen@gmail.com>
2022-11-30 11:42:04 +0100

    Renaming stuff in class-diagram
    - Logic Tier -> DAO-packages renamed
    - Data tier -> Services-packages renamed

--- Tier2/src/test/java/via/sep3/group11/tier2/Tier2ApplicationTests.java
+++ Tier2/src/test/java/via/sep3/group11/tier2/Tier2ApplicationTests.java
@@ -1,13 +1,26 @@
 package via.sep3.group11.tier2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import via.sep3.group11.tier2.businessLogic.HostLogic;
 
 @SpringBootTest
 class Tier2ApplicationTests {
 
+    @Autowired
+    HostLogic hostLogic;
+
+    /*
+
+    @Test void contextLoads() {
+    }
+
+     */
+
     @Test
-    void contextLoads() {
+    void createHostTest() {
+      //  hostLogic.registerHost()
     }
 
 }
