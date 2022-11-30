70f1d571f5ebd74cdf65b6728999d3d732db568c
Nina Wrona <ninawrona@gmail.com>
2022-11-30 17:59:08 +0100

    DTOs work again

---
+++ Tier1/HttpClients/ClientImplementations/AgreementInterface.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DTOs;
+
+namespace HttpClients.ClientImplementations;
+
+public interface AgreementInterface
+{
+    Task<RequestAgreementDTO> RequestAgreement(RequestAgreementDTO dto);
+    Task<RespondAgreementDTO> RespondToAgreement(RespondAgreementDTO dto);
+    Task<AgreementsByHostDTO> GetAllRequestsByHostDTO(AgreementsByHostDTO dto);
+}
--- Tier1/HttpClients/ClientImplementations/HousingInterface.cs
+++ Tier1/HttpClients/ClientImplementations/HousingInterface.cs
@@ -5,5 +5,6 @@ namespace HttpClients.ClientImplementations;
 public interface HousingInterface
 {
     Task<HousingDTO> AddHousingAsync(HousingCreationDTO dto);
-    Task<HousingListDTO> 
+    Task<HousingListDTO> GetAvailableHousing();
+    Task<HousingDTO> RemoveHousing(HousingIdDTO dto);
 }
