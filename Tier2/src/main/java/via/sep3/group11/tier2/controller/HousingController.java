d0b3bd19f37067d4b2b17c0bee0c1f9226988b80
Ignas Druskinis <ignasdruskinis@gmail.com>
2022-12-02 18:07:05 +0100

    housingcontroller
    ..

--- Tier2/src/main/java/via/sep3/group11/tier2/controller/HousingController.java
+++ Tier2/src/main/java/via/sep3/group11/tier2/controller/HousingController.java
@@ -18,53 +18,53 @@ import via.sep3.group11.tier2.shared.exceptions.ValidationException;
 @RequestMapping("/api")
 public class HousingController {
     /**
+     * Housing controller
      *
-     *  Housing controller
      * @author Group 11
      * @version 28/11/22
      */
     HostInterface hostInterface;
     HousingInterface housingInterface;
 
-    public HousingController(HostInterface hostInterface, HousingInterface housingInterface)
-    {
+    public HousingController(HostInterface hostInterface, HousingInterface housingInterface) {
         this.hostInterface = hostInterface;
         this.housingInterface = housingInterface;
     }
 
     /**
-     *  Adds housing !
+     * Adds housing !
+     *
      * @param housing
      * @return
      */
     @CrossOrigin
-    @PostMapping(value ="/housing",produces ={MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Housing> addHousing(@RequestBody HousingCreationDTO housing){
+    @PostMapping(value = "/housing", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Housing> addHousing(@RequestBody HousingCreationDTO housing) {
 
-        try{
+        try {
             Housing created = hostInterface.addHousing(housing);
             return new ResponseEntity<>(created, HttpStatus.CREATED);
-        }
-        catch (IllegalArgumentException | ValidationException e)
-        {
+        } catch (IllegalArgumentException | ValidationException e) {
             System.out.println(e.getMessage());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        public ResponseEntity<HousingListDTO> getAvailableHousing(@RequestBody HousingListDTO dto){
-            try{
+        public ResponseEntity<HousingListDTO> getAvailableHousing (@RequestBody HousingListDTO dto){
+            try {
                 housingInterface.getAvailableHousing(dto);
-                return new ResponseEntity<>(dto,HttpStatus.CREATED);
+                return new ResponseEntity<>(dto, HttpStatus.CREATED);
+            } catch(Exception e){
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
-        public ResponseEntity<HousingDTO> removeHousing(@RequestBody HousingIdDTO dto){
-            try{
+        public ResponseEntity<HousingDTO> removeHousing (@RequestBody HousingIdDTO dto){
+            try {
                 housingInterface.removeHousing(dto);
-                return new ResponseEntity<>(dto,HttpStatus.CREATED);
+                return new ResponseEntity<>(dto, HttpStatus.CREATED);
+            } catch (Exception e){
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
     }
-
 }
