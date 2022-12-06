17e63be7f2dfebbc1d9543c73f1199ee8c4a0371
Ignas Druskinis <ignasdruskinis@gmail.com>
2022-12-06 11:00:08 +0100

    Agreements
    working on agreements

--- Tier1/Shared/Domain/Agreement.cs
+++ Tier1/Shared/Domain/Agreement.cs
@@ -2,5 +2,37 @@
 
 public class Agreement
 {
-    
+    public long AgreementId { get; set; }
+
+    public Date Date { get; set; }
+
+    public Refugee Refugee { get; set; }
+
+    public Housing Housing { get; set; }
+
+    public Host Host { get; set; }
+
+    public bool Accepted { get; set; }
+
+    public Agreement(long agreementId, Date date, Refugee refugee, Housing housing, Host host, bool accepted)
+    {
+        AgreementId = agreementId;
+        Date = date;
+        Refugee = refugee;
+        Housing = housing;
+        Host = host;
+    }
+    public Agreement(Date date, Refugee refugee, Housing housing, Host host, bool accepted)
+    {
+        Date = date;
+        Refugee = refugee;
+        Housing = housing;
+        Host = host;
+        Accepted = accepted;
+    }
+
+    public Agreement()
+    {
+        
+    }
 }
