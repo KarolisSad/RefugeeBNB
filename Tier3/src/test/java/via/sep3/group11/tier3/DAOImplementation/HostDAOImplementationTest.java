89f9115724d0a851103274e53c71800d9168420e
karolissad <sadeckas.karolisx@gmail.com>
2022-12-06 19:13:57 +0100

    Updated application.properties

---
+++ Tier3/src/test/java/via/sep3/group11/tier3/DAOImplementation/HostDAOImplementationTest.java
@@ -0,0 +1,22 @@
+package via.sep3.group11.tier3.DAOImplementation;
+
+import org.junit.jupiter.api.Test;
+
+class HostDAOImplementationTest {
+
+    @Test
+    void createHost() {
+    }
+
+    @Test
+    void getHostByEmail() {
+    }
+
+    @Test
+    void getHostByHousingId() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+}
